1335291eb75511f5477923a68ae3ea0dd3003cf3
yareyynn <158135512+yareyynn@users.noreply.github.com>
2024-01-30 10:40:08 +0300

    Update ft_strlen.c
    imdat

--- ex06/ft_strlen.c
+++ ex06/ft_strlen.c
@@ -1,1 +1,14 @@
-g
+int ft_strlen(char *str)
+{
+    int counter;
+    int i;
+
+    counter = 0;
+    i = 0;
+    while (str[i] != '/0')
+    {
+        counter++;
+        i++;
+    }
+    return counter;
+}
